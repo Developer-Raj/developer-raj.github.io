450218151fbebe84452008d1497318bf17dfcbc5
Raj Gh <59818709+Developer-Raj@users.noreply.github.com>
2020-01-16 18:14:09 +0530

    Add files via upload

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -2,19 +2,23 @@ var hamBtn = document.querySelector(".hamburger-menu");
 var navbar = document.querySelector(".navbar");
 var is_clicked = false;
 
-window.onresize = () => {
-    if(window.screen.width <= 460){
+function ham() {
+    if(is_clicked){
+        is_clicked = false;
         navbar.style.height = "0px";
-        hamBtn.onclick = () => {
-            if(is_clicked){
-                is_clicked = false;
-                navbar.style.height = "0px";
-            } else{
-                is_clicked = true;
-                navbar.style.height = "211px";
-            }
+    } else{
+        is_clicked = true;
+        navbar.style.height = "211px";
+    }
+}
+
+hamBtn.onclick = () => {
+    ham();
+    window.onresize = () => {
+        if(window.screen.width <= 460){
+            navbar.style.height = "0px";
+        } else {
+            navbar.style.height = "50px";
         }
-    } else {
-        navbar.style.height = "50px";
     }
 }
