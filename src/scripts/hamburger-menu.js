ccbc8781c45706914c190204ab629d2ecbff9121
Developer-Raj <raajjj.gh@gmail.com>
2020-01-16 15:11:59 +0530

    fixed some bugs with hamburger

--- src/scripts/hamburger-menu.js
+++ src/scripts/hamburger-menu.js
@@ -1,25 +1,24 @@
 var hamBtn = document.querySelector(".hamburger-menu");
 var is_clicked = false;
 
-window.onresize = function() {
+window.onload = function() {
+
+    function hamOpen(){
+        if(is_clicked){
+            document.querySelector(".navbar").style.height = "0px";
+            is_clicked = false;
+        } else {
+            console.log("working");
+            is_clicked = true;
+            document.querySelector(".navbar").style.height = "211px";
+        }
+    }
+
     if(window.screen.width <= 460){
-        document.querySelector(".navbar").style.height = "0px";
         hamBtn.onclick = function() {
-            if(is_clicked){
-                document.querySelector(".navbar").style.height = "0px";
-                is_clicked = false;
-            } else {
-                is_clicked = true;
-                document.querySelector(".navbar").style.height = "211px";
-            }
+            hamOpen();
         };
     } else {
         document.querySelector(".navbar").style.height = "50px";
-    }
-
-    if(window.screen.width <= 360){
-        this.hamBtn.style.transform = "scale(0.8)";
-    } else {
-        this.hamBtn.style.transform = "scale(1)";
     }
 };
